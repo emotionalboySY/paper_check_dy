af030ffe84e4384099f4599e223bb01d2aadc3e4
emotionalboy_SY <yoppyopp@naver.com>
2023-10-30 01:51:58 +0900

    feat: add content_distribution

---
+++ lib/content_distribution.dart
@@ -0,0 +1,131 @@
+import 'package:flutter/material.dart';
+
+class ContentDistribution extends StatefulWidget {
+  const ContentDistribution({super.key});
+
+  @override
+  State<ContentDistribution> createState() => _ContentDistributionState();
+}
+
+class _ContentDistributionState extends State<ContentDistribution> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const SizedBox(
+          height: 30,
+          child: Align(
+            alignment: Alignment.centerLeft,
+            child: Padding(
+              padding: EdgeInsets.only(left: 10.0),
+              child: Text("배부)"),
+            ),
+          ),
+        ),
+        SizedBox(
+          height: 180,
+          width: double.infinity,
+          child: DecoratedBox(
+            decoration: BoxDecoration(
+              color: Colors.grey.shade400,
+            ),
+            child: Column(
+              children: [
+                ListView(
+                  shrinkWrap: true,
+                  children: List<Widget>.generate(
+                    3,
+                    (index) {
+                      return Padding(
+                        padding: const EdgeInsets.only(bottom: 2.0),
+                        child: SizedBox(
+                          height: 35,
+                          width: double.infinity,
+                          child: Row(
+                            children: [
+                              Expanded(
+                                child: SizedBox(
+                                  height: 30,
+                                  width: double.infinity,
+                                  child: DecoratedBox(
+                                    decoration: BoxDecoration(
+                                      color: Colors.grey.shade500,
+                                    ),
+                                    child: Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.start,
+                                      children: [
+                                        Padding(
+                                          padding: const EdgeInsets.symmetric(
+                                              horizontal: 5.0),
+                                          child: SizedBox(
+                                            width: 20,
+                                            height: 20,
+                                            child: DecoratedBox(
+                                              decoration: BoxDecoration(
+                                                color: Colors.grey.shade400,
+                                              ),
+                                              child: Center(
+                                                child: Text(
+                                                  "$index.",
+                                                ),
+                                              ),
+                                            ),
+                                          ),
+                                        ),
+                                        Text(
+                                          "distribution item index #$index",
+                                        )
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              Align(
+                                alignment: Alignment.centerRight,
+                                child: IconButton(
+                                  padding: EdgeInsets.zero,
+                                  onPressed: () {},
+                                  icon: const Icon(
+                                    Icons.close,
+                                    color: Colors.black,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.center,
+                  child: SizedBox(
+                    width: 30,
+                    height: 30,
+                    child: DecoratedBox(
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Colors.grey.shade500,
+                      ),
+                      child: InkWell(
+                        onTap: () {},
+                        child: const Center(
+                          child: Icon(
+                            Icons.add,
+                            size: 30,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
--- lib/content_main.dart
+++ lib/content_main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:paper_check_dy/content_distribution.dart';
 import 'package:paper_check_dy/content_selector_title.dart';
 import 'package:paper_check_dy/content_select_semester.dart';
 import 'package:paper_check_dy/content_selector_list.dart';
@@ -19,6 +20,7 @@ class _MainState extends State<Main> {
           ContentSemester(),
           ContentSelectorTitle(),
           ContentSelectorList(),
+          ContentDistribution(),
         ],
       ),
     );
